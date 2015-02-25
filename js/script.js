051275cea9de21649f9b2196b6533739b6a8a98b
DavidYourNeighbor <dcnaber@gmail.com>
2015-02-25 00:42:05 +0000

    MP1 webpage up and running at least

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
+$('#introduction, #middlesection, #pastproductions, #middlesection2, #gettoknowus').on('click', function(e){
+   e.preventDefault();
+    var scrollToThis;
+    if($(this).get(0).href == 'introduction') {
+        scrollToThis = $('introduction');
+    }
+    else if($(this).get(0).href == 'middlesection'){
+        scrollToThis = $('middlesection');
+    }
+    else if($(this).get(0).href == 'middlesection2'){
+        scrollToThis = $('middlesection2');
+    }
+    else if($(this).get(0).href == 'pastproductions'){
+        scrollToThis = $('pastproductions');
+    }
+    else if($(this).get(0).href == 'gettoknowus'){
+        scrollToThis = $('gettoknowus');
+    }
+    $('html, body').stop().animate({scrollTop: scrollToThis.offset().top}, 300);
+});
 
-function highlightThis(event) {
-    //event.stopPropagation();
-  
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
+
+var $items = $(".carouselcontainer div");
+$("button").click(function(){
+    index = $(".carouselcontainer div:visible").index();
+    $items.hide().eq(
+        (index + $items.length + ($(this).attr("data-mod") * 1)) % $items.length).css("display", "inline-block");
+});
